8167811d23cb842d99eb3aa1b639a935d3737e7c
Vicente Lopez <vicentel0809@gmail.com>
2026-08-07 15:44:22 -0700

    Update TireDeflection

--- task4.m
+++ task4.m
@@ -10,7 +10,7 @@ requirements = struct( ...
     "RMSAcceleration", 3.0, ...       % m/s^2
     "PeakAcceleration", 8.0, ...      % m/s^2
     "SuspensionTravel", 0.080, ...    % m
-    "TireDeflection", 0.050);         % m
+    "TireDeflection", 0.080);         % m
 
 % Run automated test runner
 summaryTable = runAllTests(modelName, requirements);
